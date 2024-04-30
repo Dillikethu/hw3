414e5ba7c9871476f1372dacde1095a32ee5f1fc
Dillikethu <163803225+Dillikethu@users.noreply.github.com>
2024-04-29 22:26:27 -0500

    Update dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,25 +1,37 @@
-function findDefinition() {
-    const searchTerm = document.getElementById("search-input").value.trim().toLowerCase();
-    const resultContainer = document.getElementById("result-list");
-    resultContainer.innerHTML = "";
 
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.title === "No Definitions ") {
-                resultContainer.innerHTML = "<li>No entries located</li>";
-            } else {
-                const matchingEntries = data.filter(entry => entry.word.toLowerCase() === searchTerm);
-                if (matchingEntries.length > 0) {
-                    matchingEntries.forEach(entry => {
-                        const definition = entry.meanings[0].definitions[0].definition;
-                        resultContainer.innerHTML += `<li>${entry.word}: ${definition}</li>`;
-                    });
-                } else {
-                    resultContainer.innerHTML = "<li>No exact match found</li>";
-                }
-            }
-        });
-}
+<!DOCTYPE html>
+<html lang="en">
 
-document.getElementById("search-input").addEventListener("input", findDefinition);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dictionary App</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+    <header>
+
+        <nav>
+            <p>Dictionary</p>
+
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="./blog.html">Blog</a></li>
+                <li><a href="./about.html">About</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div id="inputBoxContainer">
+            <input type="text" id="inputBox" placeholder="Enter a word">
+        </div>
+        <ol id="nounList"></ol>
+    </main>
+
+    <script src="../js/dictionary.js"></script>
+</body>
+
+</html>
